c0f9ac518fdb7b92aaf086be48edd28743f7f1d8
oran sk <oransk96@gmail.com>
2024-11-05 20:52:51 +0530

    add new

--- JS5/script.js
+++ JS5/script.js
@@ -1,21 +1,45 @@
-function one() {
-  const username = "oran"; // Defined within `one()` scope
-  function two() {
-    const website = "youtube"; // Defined within `two()` scope
-    console.log(username); // Accessing `username` from `one()` scope
-  }
-  two(); // Calls the `two()` function
+// Selecting elements
+const userScoreSpan = document.getElementById("user-score");
+const computerScoreSpan = document.getElementById("computer-score");
+const resultText = document.getElementById("result");
+const choices = document.querySelectorAll(".choice");
+
+// Initial scores
+let computerScore = 0;
+let userScore = 0;
+
+// Choices array for the computer
+const arryComputer = ["rock", "paper", "scissors"];
+
+// Function for computer to pick a random choice
+function computer() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    const newA = arryComputer[randomNumber];
+    return newA; // return the choice for use in playGame
 }
- one()// Calls the `one()` function
 
- if(true){
-    const name = "sabnam"
-    if(name === "sabnam"){
-        const husband = "oran"
-        console.log(name + " " + husband)
+// Function to play the game
+function playGame(userChoice) {
+    const computerPlay = computer(); // get computer choice
+
+    if (computerPlay === userChoice) {
+        resultText.innerText = `It's a draw! You both chose ${userChoice}.`;
+    } else if (
+        (userChoice === 'rock' && computerPlay === 'scissors') ||
+        (userChoice === 'scissors' && computerPlay === 'paper') ||
+        (userChoice === 'paper' && computerPlay === 'rock')
+    ) {
+        userScore++;
+        userScoreSpan.innerText = userScore;
+        resultText.innerText = `You win! ${userChoice} beats ${computerPlay}.`;
+    } else {
+        computerScore++;
+        computerScoreSpan.innerText = computerScore;
+        resultText.innerText = `You lose! ${computerPlay} beats ${userChoice}.`;
     }
- };
+}
 
-
- //++++++++++++++++++++
- 
+// Adding event listeners to each choice
+choices.forEach(choice => {
+    choice.addEventListener('click', () => playGame(choice.id));
+});
