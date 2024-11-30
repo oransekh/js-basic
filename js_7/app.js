9f883a1509191c7578aef75120f4e590eabdf46a
oran sk <oransk96@gmail.com>
2024-11-30 11:03:49 +0530

    First release of Hello World!

--- js_7/app.js
+++ js_7/app.js
@@ -19,30 +19,3 @@ closeButton.addEventListener("click", (e) => {
   }, 1000);
 });
 
-// Get references to the toggle button and the mobile menu
-const menuToggleButton = document.getElementById("menu-toggle");
-const mobileMenu = document.getElementById("mobile-menu");
-
-// Toggle the menu on button click
-menuToggleButton.addEventListener("click", () => {
-  console.log("Button clicked");
-
-  // Toggle the visibility and translate classes
-  if (mobileMenu.classList.contains("-translate-y-full")) {
-    mobileMenu.classList.remove("hidden", "-translate-y-full");
-    mobileMenu.classList.add("translate-y-0");
-  } else {
-    mobileMenu.classList.add("-translate-y-full");
-    setTimeout(() => mobileMenu.classList.add("hidden"), 700); // Ensure animation completes before hiding
-    mobileMenu.classList.remove("translate-y-0");
-  }
-});
-
-// Optional: Close menu if clicked outside (e.g., on a menu item)
-mobileMenu.addEventListener("click", (e) => {
-  e.preventDefault();
-  
-  mobileMenu.classList.add("-translate-y-full");
-  setTimeout(() => mobileMenu.classList.add("hidden"), 700);
-  mobileMenu.classList.remove("translate-y-0");
-});
